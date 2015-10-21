6d95b5fdbdcb7a29793b034a84a61f5a43a448de
YueyangZhang <billzyy@gmail.com>
2015-10-20 20:56:29 -0400

    bucketSort

---
+++ BucketSort/bucketSort.java
@@ -0,0 +1,54 @@
+package BucketSort;
+
+import java.util.Random;
+
+public class bucketSort {
+	static int[] bucketSort(int[] nums) {
+		if (nums == null || nums.length <= 1) {
+			return nums;
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for (int num: nums) {
+			max = Math.max(max, num);
+		}
+		
+		int[] bucket = new int[max + 1];
+		int[] result = new int[nums.length];
+		for (int num: nums) {
+			bucket[num]++;
+		}
+		
+		int start = 0;
+		for(int i = 0; i < bucket.length; i++) {
+			for (int j = 0; j < bucket[i]; j++) {
+				result[start++] = i;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+	    int n = 20;
+	    int[] sequence = new int[n];
+	 
+	    for (int i = 0; i < n; i++) {
+	    	sequence[i] = Math.abs(random.nextInt(100));
+	    }
+	    
+	    int[] result = bucketSort(sequence);
+	    
+	    System.out.println("Original:");
+	    for (int i = 0; i < n; i++) {
+	    	System.out.print(sequence[i] + " ");
+	    }
+	    System.out.println();
+	    
+	    System.out.println("Sorted:");
+	    for (int i = 0; i < n; i++) {
+	    	System.out.print(result[i] + " ");
+	    }
+	}
+}
