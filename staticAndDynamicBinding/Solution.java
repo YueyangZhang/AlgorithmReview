996ba358feb706c725d8e181af7770b67f32b9e4
YueyangZhang <billzyy@gmail.com>
2015-11-02 20:30:08 -0500

    bind

--- staticAndDynamicBinding/Solution.java
+++ staticAndDynamicBinding/Solution.java
@@ -1,5 +1,10 @@
 package staticAndDynamicBinding;
 
+/*
+ * When type of the object is determined at compiled time(by the compiler), it is known as static binding.
+ * If there is any private, final or static method in a class, there is static binding.
+ * When type of the object is determined at run-time, it is known as dynamic binding.
+ */
 public class Solution {
 
 	public static void main(String[] args) {
